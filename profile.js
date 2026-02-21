463172c0fe7c04e51e3f7989e79a86e4a6662ab1
abdullax13 <48449922+abdullax13@users.noreply.github.com>
2026-02-21 23:46:09 +0300

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder } = require("discord.js");
 const Player = require("./Player");
 const { generateProfileCard } = require("./profileCard");
 module.exports = {
