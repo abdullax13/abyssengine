9cdce31b1242016c07a6973600f2f800acd2b147
abdullax13 <48449922+abdullax13@users.noreply.github.com>
2026-02-21 20:51:54 +0300

    Update logger.js

--- logger.js
+++ logger.js
@@ -1,9 +1,8 @@
-function ts() {
+function timestamp() {
   return new Date().toISOString();
 }
 
 module.exports = {
-  info: (msg) => console.log(`[${ts()}] [INFO] ${msg}`),
-  warn: (msg) => console.warn(`[${ts()}] [WARN] ${msg}`),
-  error: (msg) => console.error(`[${ts()}] [ERROR] ${msg}`)
+  info: (msg) => console.log(`[${timestamp()}] [INFO] ${msg}`),
+  error: (msg) => console.error(`[${timestamp()}] [ERROR] ${msg}`)
 };
